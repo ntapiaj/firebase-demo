38465400a1a6e6dd7a7a521b91eab7c371fcfa60
Nick Steven Steven <106779652+ntapiaj@users.noreply.github.com>
2022-06-08 21:01:36 +0000

    own firebase

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
   // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBUb1DD2a3W4dDxhM2uIBVesHb4wqksQSk",
-    authDomain: "fir-demo-5ad62.firebaseapp.com",
-    projectId: "fir-demo-5ad62",
-    storageBucket: "fir-demo-5ad62.appspot.com",
-    messagingSenderId: "327286699446",
-    appId: "1:327286699446:web:6a3cadeadc391df6e23d17",
-    measurementId: "G-0E0YDR4TQ1"
-  };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAg6raW7bi6SsS1I7JBzWWMo-qmabFp59c",
+  authDomain: "fir-demo-115ef.firebaseapp.com",
+  projectId: "fir-demo-115ef",
+  storageBucket: "fir-demo-115ef.appspot.com",
+  messagingSenderId: "686623492429",
+  appId: "1:686623492429:web:96d4c4dcc531cd40828f04",
+  measurementId: "G-NK0P4C56H5"
+};
+
 
   // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
